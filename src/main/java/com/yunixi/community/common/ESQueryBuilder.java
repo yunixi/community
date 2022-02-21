4d9d438cf10d6614905a23445b02d52c941e96f6
yunixi <jyjy2197@gmail.com>
2022-02-21 23:47:25 +0900

    [20220221] ElasticSearch term 쿼리 추가

--- src/main/java/com/yunixi/community/common/ESQueryBuilder.java
+++ src/main/java/com/yunixi/community/common/ESQueryBuilder.java
@@ -1,8 +1,16 @@
 package com.yunixi.community.common;
 
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
 public class ESQueryBuilder {
 
-    public String setTermQuery() {
-        return "";
+    public SearchSourceBuilder setTermQuery() {
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.termQuery("user", "kimchy"));
+        sourceBuilder.from(0);
+        sourceBuilder.size(5);
+
+        return sourceBuilder;
     }
 }
